06fdc5b1eadbe95ab4cfa52620f583dd63bf438a
Gabe Medrash <gabeme@alleninstitute.org>
2019-04-17 17:50:05 -0700

    [ACVIZCOMM-58] Start to tie functional pieces together

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
 import * as React from "react";
 
-export default class App extends React.Component {
+import PrimaryMediaByPageGroup from "./components/PrimaryMediaByPageGroup";
+import Page from "./essay/entity/Page";
+
+interface AppProps {
+    activePage: Page;
+    pagesBinnedByLayout: Page[][];
+    pagesBinnedByMedia: Page[][];
+}
+
+export default class App extends React.Component<AppProps, {}> {
     public render(): JSX.Element {
-        return <div>Hello world</div>;
+        return <>{this.renderPrimaryMedia()}</>;
+    }
+
+    private renderPrimaryMedia(): JSX.Element[] {
+        const { activePage, pagesBinnedByMedia } = this.props;
+
+        return pagesBinnedByMedia
+            .filter((bin: Page[]) => {
+                // we currently do not support anything other than video as primary media
+                const mediaReferenceSharedInBin = bin[0].media.reference;
+                const isVideo = mediaReferenceSharedInBin.type === "video";
+
+                // if isVideo, return true
+                // else, log the attempt to render something other than video as primary media
+                // and return false (exotic comma operator in action)
+                return (
+                    isVideo ||
+                    (console.log(
+                        "Attempted to return something other than video as primary media",
+                        mediaReferenceSharedInBin
+                    ),
+                    false)
+                );
+            })
+            .map((bin: Page[]) => (
+                <PrimaryMediaByPageGroup
+                    key={bin[0].media.mediaId}
+                    activePage={activePage}
+                    pageGroup={bin}
+                />
+            ));
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,44 @@ import "core-js/es6/promise";
 import "core-js/es6/set";
 
 import * as React from "react";
-import { render } from "react-dom";
+import * as ReactDOM from "react-dom";
 
-import { APP_ID } from "./constants";
 import App from "./App";
+import { APP_ID } from "./constants";
+import essay from "./essay";
+import InteractionController from "./InteractionController";
 
-render(<App />, document.getElementById(APP_ID));
+const appRoot = document.getElementById(APP_ID);
+
+if (!appRoot) {
+    throw new Error(
+        `The DOM is missing the element expected to contain the application: #${APP_ID}`
+    );
+}
+
+function render() {
+    ReactDOM.render(
+        <App
+            activePage={essay.activePage}
+            pagesBinnedByLayout={essay.pagesBinnedByLayout()}
+            pagesBinnedByMedia={essay.pagesBinnedByMedia()}
+        />,
+        appRoot
+    );
+}
+
+const controller = new InteractionController();
+controller.listenForInteractions(appRoot, (deltaY: number) => {
+    if (deltaY > 0) {
+        // moving down
+        essay.advance();
+    } else {
+        // moving up
+        essay.goBack();
+    }
+
+    render();
+});
+
+// kick it off
+render();
