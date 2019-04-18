1d87daf4d322e853dc9f55d8447d6abf33b48958
Gabe Medrash <gabeme@alleninstitute.org>
2019-04-17 23:18:43 -0700

    [ACVIZCOMM-58] Accept classname thru props

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,16 +1,22 @@
+import * as classNames from "classnames";
 import * as React from "react";
 
 const styles = require("./style.css");
 
 interface ImageProps {
     caption?: string;
+    className?: string;
     source: string;
 }
 
 export default function Image(props: ImageProps) {
     return (
         <figure className={styles.figure}>
-            <img className={styles.image} decoding="async" src={props.source} />
+            <img
+                className={classNames(styles.image, props.className)}
+                decoding="async"
+                src={props.source}
+            />
             {props.caption && <figcaption>{props.caption}</figcaption>}
         </figure>
     );
