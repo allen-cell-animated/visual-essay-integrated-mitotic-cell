1ba12af80533cf3bfa627660653815b7de1b914e
Megan Riel-Mehan <meganrm@gmail.com>
2019-04-23 12:33:17 -0700

    updating with new markers and text

--- src/essay/index.ts
+++ src/essay/index.ts
@@ -1,6 +1,8 @@
 import media from "./media.json";
 import Essay from "./entity/Essay";
 import introduction from "./section-1-introduction";
+import observations from "./section-2-observations";
+
 import explore from "./section-3-explore-3d-data";
 
-export default new Essay([introduction, explore], media);
+export default new Essay([introduction, observations, explore], media);
---
+++ src/essay/section-2-observations/index.ts
@@ -0,0 +1,12 @@
+// chapters
+import chapter1 from "./ch-1-redistribution-and-reorganization.json";
+import chapter2 from "./ch-2-structure-disassembly-and-recompartmentalization.json";
+import chapter3 from "./ch-3-structures-maintained-throughout-mitosis.json";
+import chapter4 from "./ch-4-early-prometaphase.json";
+import chapter5 from "./ch-5-conclusions.json";
+
+export default {
+    sectionId: "obs",
+    title: "Observations",
+    chapters: [chapter1, chapter2, chapter3, chapter4, chapter5],
+};
