4598dbab67da665fedbfb29f3ca96ad536611030
Gabe Medrash <gabeme@alleninstitute.org>
2019-05-01 00:20:18 -0700

    [ACE-120] Stub in Appendix component

---
+++ src/components/Appendix/index.tsx
@@ -0,0 +1,22 @@
+import * as classNames from "classnames";
+import * as React from "react";
+
+import { default as ImageComponent } from "../Image";
+import { InteractivePageProps } from "../InteractiveByPageGroup";
+
+const styles = require("./style.css");
+
+export default class Appendix extends React.Component<InteractivePageProps> {
+    public render(): JSX.Element {
+        const { className } = this.props;
+
+        return (
+            <section className={classNames(className, styles.section)}>
+                <ImageComponent
+                    className={styles.mockup}
+                    source="https://s3-us-west-2.amazonaws.com/staging.imsc-visual-essay.allencell.org/assets/appendix-mockup.png"
+                />
+            </section>
+        );
+    }
+}
