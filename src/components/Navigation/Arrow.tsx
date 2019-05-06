90f87cbf3f4877fe3f3fbb168648bad24d815c4c
Gabe Medrash <gabeme@alleninstitute.org>
2019-05-06 12:11:35 -0700

    [ACE-140] Render nav buttons; initial look and feel

--- src/components/Navigation/Arrow.tsx
+++ src/components/Navigation/Arrow.tsx
@@ -9,14 +9,19 @@ export enum ArrowDirection {
 }
 
 interface ArrowProps {
+    className?: string;
     direction: ArrowDirection;
     disabled: boolean;
     onClick: () => void;
 }
 
+// dimensions at which SVG was designed; scale the graphic accordingly
+const SVG_DESIGN_WIDTH = 400;
+const SVG_DESIGN_HEIGHT = 1000;
+
 const DIRECTION_TO_TRANSFORM_MAP = {
     [ArrowDirection.LEFT]: "rotate(0)", // no translation, raw SVG is already a left-pointing arrow
-    [ArrowDirection.RIGHT]: "rotate(180)",
+    [ArrowDirection.RIGHT]: `rotate(180, ${SVG_DESIGN_WIDTH / 2}, ${SVG_DESIGN_HEIGHT / 2})`, // rotate about the center of the viewbox
 };
 
 /**
@@ -24,18 +29,22 @@ const DIRECTION_TO_TRANSFORM_MAP = {
  * License: https://creativecommons.org/licenses/by-sa/4.0/.
  */
 export default function Arrow(props: ArrowProps) {
-    const { direction, disabled, onClick } = props;
+    const { className, direction, disabled, onClick } = props;
 
-    const buttonClasses = classNames(styles.button, {
-        [styles.disabled]: disabled,
-    });
+    const buttonClasses = classNames(
+        styles.button,
+        {
+            [styles.disabled]: disabled,
+        },
+        className
+    );
 
     return (
         <button className={buttonClasses} disabled={disabled} onClick={onClick} type="button">
             <svg
-                width="100%"
-                height="100%"
-                viewBox="0 0 400 1000"
+                width="16px"
+                height="19px"
+                viewBox={`0 0 ${SVG_DESIGN_WIDTH} ${SVG_DESIGN_HEIGHT}`}
                 xmlns="http://www.w3.org/2000/svg"
             >
                 <g transform={DIRECTION_TO_TRANSFORM_MAP[direction]}>
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { Page } from "../../essay/entity/BasePage";
 import Section from "../../essay/entity/Section";
 
+import Arrow, { ArrowDirection } from "./Arrow";
 import NavChapter from "./NavChapter";
 import NavSection from "./NavSection";
 import { getNavPoints } from "./selectors";
@@ -18,46 +19,58 @@ interface NavigationProps {
 }
 
 // dimensions at which SVG was designed; scale the graphic accordingly
-const VIEWBOX_WIDTH = 911;
-const VIEWBOX_HEIGHT = 66;
+const SVG_DESIGN_WIDTH = 911;
+const SVG_DESIGN_HEIGHT = 66;
 
 export default function Navigation(props: NavigationProps) {
     return (
-        <svg
-            className={classNames(styles.container, props.className)}
-            viewBox={`0 0 ${VIEWBOX_WIDTH} ${VIEWBOX_HEIGHT}`}
-            pointerEvents="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            {getNavPoints(props.sections).map((navSection) => {
-                return (
-                    <NavSection
-                        active={navSection.section === props.activePage.chapter.section}
-                        key={navSection.page.id}
-                        height={VIEWBOX_HEIGHT}
-                        label={navSection.label}
-                        onClick={props.onNavigation}
-                        page={navSection.page}
-                        translateX={navSection.translateX}
-                        width={navSection.width}
-                    >
-                        {navSection.chapters.map((navChapter) => (
-                            <NavChapter
-                                chapterIsActive={navChapter.chapter === props.activePage.chapter}
-                                key={navChapter.page.id}
-                                label={navChapter.label}
-                                onClick={props.onNavigation}
-                                page={navChapter.page}
-                                sectionIsActive={
-                                    navSection.section === props.activePage.chapter.section
-                                }
-                                translateX={navChapter.translateX}
-                                translateY={VIEWBOX_HEIGHT / 2}
-                            />
-                        ))}
-                    </NavSection>
-                );
-            })}
-        </svg>
+        <div className={classNames(styles.container, props.className)}>
+            <Arrow
+                className={classNames(styles.arrow, styles.left)}
+                direction={ArrowDirection.LEFT}
+            />
+            <svg
+                className={styles.mainNav}
+                viewBox={`0 0 ${SVG_DESIGN_WIDTH} ${SVG_DESIGN_HEIGHT}`}
+                pointerEvents="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                {getNavPoints(props.sections).map((navSection) => {
+                    return (
+                        <NavSection
+                            active={navSection.section === props.activePage.chapter.section}
+                            key={navSection.page.id}
+                            height={SVG_DESIGN_HEIGHT}
+                            label={navSection.label}
+                            onClick={props.onNavigation}
+                            page={navSection.page}
+                            translateX={navSection.translateX}
+                            width={navSection.width}
+                        >
+                            {navSection.chapters.map((navChapter) => (
+                                <NavChapter
+                                    chapterIsActive={
+                                        navChapter.chapter === props.activePage.chapter
+                                    }
+                                    key={navChapter.page.id}
+                                    label={navChapter.label}
+                                    onClick={props.onNavigation}
+                                    page={navChapter.page}
+                                    sectionIsActive={
+                                        navSection.section === props.activePage.chapter.section
+                                    }
+                                    translateX={navChapter.translateX}
+                                    translateY={SVG_DESIGN_HEIGHT / 2}
+                                />
+                            ))}
+                        </NavSection>
+                    );
+                })}
+            </svg>
+            <Arrow
+                className={classNames(styles.arrow, styles.right)}
+                direction={ArrowDirection.RIGHT}
+            />
+        </div>
     );
 }
