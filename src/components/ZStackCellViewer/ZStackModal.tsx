3c4f11ff723ccfb95d70a21c79e99f494f52363c
Megan Riel-Mehan <meganrm@gmail.com>
2019-05-07 12:09:49 -0700

    moved the url formatting down to the modal

--- src/components/ZStackCellViewer/ZStackModal.tsx
+++ src/components/ZStackCellViewer/ZStackModal.tsx
@@ -1,41 +1,48 @@
 import * as React from "react";
 import { range } from "lodash";
-import { Modal } from "antd";
+import { Modal, Divider } from "antd";
 import ZStackScroller from "z-stack-scroller";
 
-import { SLICES_PER_ZSTACK } from "./constants";
+import { SLICES_PER_ZSTACK, ZSTACK_IDS, MITOTIC_PHASES_DIR } from "./constants";
 import {
     MITOTIC_STAGE_NAMES,
     STRUCTURE_NAMES,
-    LABELED_STRUCTURE_NAME_MAP,
+    GENE_ID_MAP,
     MITOTIC_STAGES_MAP,
 } from "../../constants/cell-viewer-apps";
+import { ASSETS_FOLDER } from "../../constants";
 
 const styles = require("./style.css");
 
 interface ZStackModalProps {
     closeModal: () => void;
-    modalVisible: boolean;
-    zstacknameComposite: string;
-    zstacknameChannel3: string;
-    selectedGeneId: keyof typeof LABELED_STRUCTURE_NAME_MAP;
-    selectedMitoticStage: keyof typeof MITOTIC_STAGES_MAP;
+    selectedGeneId?: keyof typeof GENE_ID_MAP;
+    selectedMitoticStage?: keyof typeof MITOTIC_STAGES_MAP;
 }
 
 const ZStackModal: React.FunctionComponent<ZStackModalProps> = ({
     closeModal,
-    modalVisible,
-    zstacknameComposite,
-    zstacknameChannel3,
     selectedGeneId,
     selectedMitoticStage,
 }: ZStackModalProps) => {
-    const proteinId = LABELED_STRUCTURE_NAME_MAP[selectedGeneId];
+    if (!selectedGeneId || !selectedMitoticStage) {
+        return null;
+    }
+
+    const proteinId = GENE_ID_MAP[selectedGeneId];
+    const stage = MITOTIC_STAGES_MAP[selectedMitoticStage];
+    const protein = GENE_ID_MAP[selectedGeneId];
+    const cellid = ZSTACK_IDS[stage][protein];
+    const stageDir = MITOTIC_PHASES_DIR[stage];
+    const zstacknameComposite = `${ASSETS_FOLDER}/mitotic_png/${stageDir}/${selectedGeneId}_${cellid}/${selectedGeneId}_${cellid}_composite/${selectedGeneId}_${cellid}_raw.ome.cropped_composite_RGB_`;
+    const zstacknameChannel3 = `${ASSETS_FOLDER}/mitotic_png/${stageDir}/${selectedGeneId}_${cellid}/${selectedGeneId}_${cellid}_channel3/${selectedGeneId}_${cellid}_raw.ome.cropped_channel3_RGB_`;
 
     return (
         <Modal
-            title="Z-stack cell viewer"
-            visible={modalVisible}
+            title={`Z-stack view of ${MITOTIC_STAGE_NAMES[selectedMitoticStage]} ${
+                STRUCTURE_NAMES[proteinId]
+            } visualized via ${selectedGeneId}`}
+            visible={true} // this is being controlled by the selection of geneId and stage, so no need to keep track of this seperately.
             centered
             width="50%"
             footer={null}
@@ -57,9 +64,11 @@ const ZStackModal: React.FunctionComponent<ZStackModalProps> = ({
                     <span key="membrane" className="membrane">
                         Cell membrane
                     </span>,
+                    <Divider key="divider-1" type="vertical" />,
                     <span key="dna" className="dna">
                         DNA
                     </span>,
+                    <Divider key="divider-2" type="vertical" />,
                     <span key="selected-gene">{selectedGeneId}</span>,
                 ]}
                 captionLeft={`Labeled ${selectedGeneId}`}
