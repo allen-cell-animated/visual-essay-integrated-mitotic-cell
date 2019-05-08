c84c3f6bdd8215af90b262945e961004cb790e20
Gabe Medrash <gabeme@alleninstitute.org>
2019-05-08 13:12:06 -0700

    New content for sections 1 and 2

--- src/essay/section-2-observations/index.ts
+++ src/essay/section-2-observations/index.ts
@@ -2,11 +2,12 @@
 import chapter1 from "./ch-1-redistribution-and-reorganization.json";
 import chapter2 from "./ch-2-structure-disassembly-and-recompartmentalization.json";
 import chapter3 from "./ch-3-structures-maintained-throughout-mitosis.json";
-import chapter4 from "./ch-4-early-prometaphase.json";
-import chapter5 from "./ch-5-conclusions.json";
+import chapter4 from "./ch-4-multi-localization.json";
+import chapter5 from "./ch-5-early-prometaphase.json";
+import chapter6 from "./ch-6-summary.json";
 
 export default {
     sectionId: "obs",
     title: "Observations",
-    chapters: [chapter1],
+    chapters: [chapter1, chapter2, chapter3, chapter4, chapter5, chapter6],
 };
