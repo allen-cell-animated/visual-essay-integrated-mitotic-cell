71e4b61c11c59061d5d91bf8232e8726a529b2c9
Gabe Medrash <gabeme@alleninstitute.org>
2019-05-13 11:33:05 -0700

    [ACE-147] Basic tests for calculating vector magnitude

--- src/util/test/index.test.ts
+++ src/util/test/index.test.ts
@@ -95,5 +95,48 @@ describe("General utilities", () => {
                 });
             });
         });
+
+        describe("magnitude", () => {
+            const testCases = [
+                {
+                    start: new Coordinate(0, 0),
+                    end: new Coordinate(45, 45),
+                    expectedMagnitude: Math.sqrt(2 * Math.pow(45, 2)),
+                },
+                {
+                    start: new Coordinate(45, 45),
+                    end: new Coordinate(90, 90),
+                    expectedMagnitude: Math.sqrt(2 * Math.pow(45, 2)),
+                },
+                {
+                    start: new Coordinate(0, 0),
+                    end: new Coordinate(45, 0),
+                    expectedMagnitude: 45,
+                },
+                {
+                    start: new Coordinate(0, 0),
+                    end: new Coordinate(0, 45),
+                    expectedMagnitude: 45,
+                },
+                {
+                    start: new Coordinate(0, 0),
+                    end: new Coordinate(0, -45),
+                    expectedMagnitude: 45,
+                },
+                {
+                    start: new Coordinate(0, 0),
+                    end: new Coordinate(0, 0),
+                    expectedMagnitude: 0,
+                },
+            ];
+
+            testCases.forEach((test) => {
+                it(`calculates accurate magnitude from ${test.start.toString()} to ${test.end.toString()}`, () => {
+                    expect(new Vector(test.start, test.end).magnitude).to.equal(
+                        test.expectedMagnitude
+                    );
+                });
+            });
+        });
     });
 });
