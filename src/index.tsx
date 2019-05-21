51753a32952c305da4a92defc15420f7670ebb88
Gabe Medrash <gabeme@alleninstitute.org>
2019-05-20 19:04:08 -0700

    [ACE-166] Weird iOS address bar hack; compose with setCSSCustomProperties into single onResize func.

--- src/index.tsx
+++ src/index.tsx
@@ -66,7 +66,39 @@ const setCSSCustomProperties = () => {
     }
 };
 
+/**
+ * On iOS in landscape mode, the address bar will sit on top of the app and never go away. This solution,
+ * taken from https://davidwalsh.name/hide-address-bar, does not in fact accomplish what the article claims it
+ * accomplishes (hiding the address bar itself), but it does functionally address our needs. It is extremely unclear why
+ * this works, but it has the effect of pushing the (correctly sized) page below the address bar.
+ *
+ * Note that this fix only seems to work on orientation change, so if the app is loaded in landscape mode,
+ * the user must flip to portrait and then back to landscape.
+ */
+const positionBelowiOSAddressBar = () => {
+    if (!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)) {
+        setTimeout(() => {
+            window.scrollTo(0, 0);
+        }, 0);
+    }
+};
+
+/**
+ * Composition of functions that should be run on resize/orientation change
+ */
+const onResize = () => {
+    setCSSCustomProperties();
+    positionBelowiOSAddressBar();
+};
+
 // kick it off
-setVideoHeight();
-window.addEventListener("resize", setVideoHeight);
+onResize();
+window.addEventListener(
+    "resize",
+    debounce(onResize, 250, {
+        leading: true,
+        trailing: true,
+    })
+);
+
 render();
