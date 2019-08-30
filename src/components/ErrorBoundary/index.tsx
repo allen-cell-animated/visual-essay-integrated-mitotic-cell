ad9d0ecf91e40833adbc5d788491b41045b9ace5
Gabe Medrash <gabeme@alleninstitute.org>
2019-08-30 11:44:52 -0700

    [ACVIZCOMM-205] No more need for custom tracking code now that GTM can accomplish everything

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -1,14 +1,10 @@
 import * as log from "loglevel";
 import * as React from "react";
 
-import tracking from "../../util/tracking";
-
-const tracker = tracking.getTracker();
-
 type ErrorBoundaryProps = React.PropsWithChildren<{}>;
 
 /**
- * Simple wrapper component for logging and tracking errors that happen as part of a React Component tree's
+ * Simple wrapper component for logging errors that happen as part of a React Component tree's
  * lifecycle. Per the documentation (https://reactjs.org/docs/error-boundaries.html), this will not catch errors raised
  * by event handlers, async code, or errors thrown within this component.
  *
@@ -17,7 +13,6 @@ type ErrorBoundaryProps = React.PropsWithChildren<{}>;
 export default class ErrorBoundary extends React.Component<ErrorBoundaryProps> {
     public componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
         log.error(error.message, errorInfo.componentStack);
-        tracker.trackException(error, true);
     }
 
     public render() {
